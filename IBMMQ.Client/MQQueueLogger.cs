0c9d7b335e7d692fe29e8b859390cb3f322f7017
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2022-02-16 03:12:23 +0530

    re add log4net libarary

--- IBMMQ.Client/MQQueueLogger.cs
+++ IBMMQ.Client/MQQueueLogger.cs
@@ -1,12 +1,8 @@
 ﻿using log4net;
 using log4net.Config;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IBMMQ.Client
 {
